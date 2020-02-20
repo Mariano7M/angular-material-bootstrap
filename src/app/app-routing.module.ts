64af990ee60844564f91e9b7b788af06eb033c94
Marinho <swipeappsteam@gmail.com>
2020-02-19 22:53:50 -0600

    Added lazy components to app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InvoiceSendComponent } from './components/shared/invoice-send/invoice-send.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'invoice',
+    component: InvoiceSendComponent
+  },
+  {
+    path: 'lazy-one',
+    loadChildren: () => import('./modules/one-lazy.module').then(m => m.OneLazyModule)
+  },
+  {
+    path: 'lazy-two',
+    loadChildren: () => import('./modules/lazy-two.module').then(m => m.LazyTwoModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
