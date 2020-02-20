48b0be7ed81b8ff5f56196cf7273a49c9b1e3c14
Marinho <swipeappsteam@gmail.com>
2020-02-19 22:51:15 -0600

    Added logic on alertDialogService to show and handle the alertDialogComponent

--- src/app/shared/alert-dialog/alert-dialog.service.ts
+++ src/app/shared/alert-dialog/alert-dialog.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { AlertDialogComponent } from './alert-dialog.component';
+import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertDialogService {
+  dialogRef: MatDialogRef<AlertDialogComponent>;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  open(options) {
+    this.dialogRef = this.dialog.open(AlertDialogComponent, {
+      data: {
+        title: options.title,
+        message: options.message,
+        cancelText: options.cancelText,
+        confirmText: options.confirmText,
+        disableClose: options.disableClose
+      }
+    });
+  }
+
+  confirmed(): Observable<any> {
+
+    return this.dialogRef.afterClosed().pipe(take(1), map(res => {
+      return res;
+    }
+    ));
+  }
 }
