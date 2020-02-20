e3e44b6cab850895736673121a3bc29c970369bc
Marinho <swipeappsteam@gmail.com>
2020-02-19 22:52:15 -0600

    Added AlertDialogComponent to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [AlertDialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ], exports: [
+    AlertDialogComponent
+  ],
+  entryComponents: [
+    AlertDialogComponent
   ]
 })
 export class SharedModule { }
