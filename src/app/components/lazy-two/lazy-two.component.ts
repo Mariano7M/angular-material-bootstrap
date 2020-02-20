32a6c0bf15e181415ef4798a415948a90a4a2b24
Marinho <swipeappsteam@gmail.com>
2020-02-19 22:57:30 -0600

    Added logic to show the alertDialog on two different components

--- src/app/components/lazy-two/lazy-two.component.ts
+++ src/app/components/lazy-two/lazy-two.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertDialogService } from 'src/app/shared/alert-dialog/alert-dialog.service';
 
 @Component({
   selector: 'app-lazy-two',
@@ -7,12 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LazyTwoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertDialogService: AlertDialogService
+  ) { }
 
   ngOnInit() {
   }
 
   openDialog(): void {
     console.log('lazy two button');
+    const options = {
+      title: 'Leave page on lazy two?',
+      message: 'By leaving this page you will permanently lose your form changes.',
+      cancelText: 'CANCEL',
+      confirmText: 'YES, LEAVE PAGE',
+      disableClose: false
+    };
+
+    this.alertDialogService.open(options);
+
+    this.alertDialogService.confirmed().subscribe(confirmed => {
+      if (confirmed) {
+        console.log('OK');
+      } else {
+        console.log('CANCEL');
+
+      }
+    });
   }
 }
--- src/app/components/lazy/lazy.component.ts
+++ src/app/components/lazy/lazy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertDialogService } from 'src/app/shared/alert-dialog/alert-dialog.service';
 
 @Component({
   selector: 'app-lazy',
@@ -7,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LazyComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertDialogService: AlertDialogService
+  ) { }
 
   ngOnInit() {
   }
 
   openDialog(): void {
     console.log('lazy one button');
+    const options = {
+      title: 'Leave page?',
+      message: 'By leaving this page you will permanently lose your form changes.',
+      cancelText: 'CANCEL',
+      confirmText: 'YES, LEAVE PAGE',
+      disableClose: true
+    };
 
+    this.alertDialogService.open(options);
+
+    this.alertDialogService.confirmed().subscribe(confirmed => {
+      if (confirmed) {
+        console.log('OK');
+      } else {
+        console.log('CANCEL');
+
+      }
+    });
   }
 }
