e11dfc207dca292ddb1b91337be139a38c6dd578
Marinho <swipeappsteam@gmail.com>
2020-02-19 22:50:19 -0600

    Added logic to alert dialog component

--- src/app/shared/alert-dialog/alert-dialog.component.ts
+++ src/app/shared/alert-dialog/alert-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-alert-dialog',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {
+      title: string,
+      message: string,
+      showConfirmText: boolean,
+      showCancelText: boolean,
+      confirmText: string,
+      cancelText: string,
+      disableClose: boolean
+    },
+    private alertDialogRef: MatDialogRef<AlertDialogComponent>) {
+    this.alertDialogRef.disableClose = this.data.disableClose;
+  }
 
   ngOnInit() {
   }
 
+  public cancel() {
+    this.close(false);
+  }
+  public close(value) {
+    this.alertDialogRef.close(value);
+  }
+  public confirm() {
+    this.close(true);
+  }
 }
